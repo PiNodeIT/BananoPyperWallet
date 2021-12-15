a8fad9388a79cc0297940bd90279c412de9674f5
PiNodeIT <95429416+PiNodeIT@users.noreply.github.com>
2021-12-15 18:29:11 +0100

    Update bananopyper.py

--- bananopyper.py
+++ bananopyper.py
@@ -1,9 +1,14 @@
+print("IT IS HIGHLY RECOMMENDED TO USE THIS SCRIPT OFFLINE")
+input("Press Enter to continue...")
+
 from nanolib import generate_seed
 from nanolib import generate_account_id
 
-seed = generate_seed()
-account_id = generate_account_id(seed, 0)
-address = account_id.replace("xrb", "ban")
-
-print("Seed:", seed)
-print("Address:", address)
+nwall = int(input("How many wallet i must generate? :\n"))
+name = input("Save as (no extension) :\n")
+for _ in range(nwall):
+    seed = generate_seed()
+    account_id = generate_account_id(seed, 0)
+    address = account_id.replace("xrb", "ban")
+    f = open((name)+".txt", 'a')
+    f.write("seed: "+(seed)+", Address: "+(address)+("\n"))
